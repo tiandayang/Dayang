ef0095034442f68ab4d4f59b81998f131e627499
田向阳 <254711559@qq.com>
2017-09-01 18:55:04 +0800

    转场动画

---
+++ Dayang/Lib/Photos/Helper/DYPhotoPreviewAnimation.swift
@@ -0,0 +1,15 @@
+//
+//  DYPhotoPreviewAnimation.swift
+//  Dayang
+//
+//  Created by 田向阳 on 2017/9/1.
+//  Copyright © 2017年 田向阳. All rights reserved.
+//
+
+import UIKit
+
+private let animationDuration = 0.18
+
+//class DYPhotoPreviewAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+//    
+//}
